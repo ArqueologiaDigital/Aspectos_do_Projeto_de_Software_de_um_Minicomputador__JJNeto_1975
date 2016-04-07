95881a48830884b86d5ce6a68ada16be3f585745
Felipe Corrêa da Silva Sanches <fsanches@metamaquina.com.br>
2016-04-07 13:40:26 -0300

    gerando um PDF

--- lowres/make_pdf.sh
+++ lowres/make_pdf.sh
@@ -9,4 +9,4 @@ echo "And now generating the PDF..."
 convert ../processadas/intro-*.lowres.jpg ../processadas/pagina-*.lowres.jpg Aspectos_do_Projeto_de_Software_de_um_Minicomputador__JJNeto_1975_lowres.pdf
 echo "done."
 
-rm ../*.lowres.jpg -f
+rm ../processadas/*.lowres.jpg -f
